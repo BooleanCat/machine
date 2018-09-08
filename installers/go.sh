f6feb984b4ed148ca19cd5ab666c516ff323dad8
Tom Godkin <thgodkin@gmail.com>
2018-09-08 18:29:13 +0100

    install go in script

---
+++ installers/go.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function rm_if_exists() {
+  path="$1"
+  if [ -f "$path" ]; then
+    rm -rf "$path"
+  fi
+}
+
+go_version="go1.11.linux-amd64"
+
+rm_if_exists /usr/local/go
+rm_if_exists "/tmp/${go_version}.tar.gz"
+
+(
+  cd "/tmp"
+  curl -R -O "https://dl.google.com/go/${go_version}.tar.gz"
+  sudo tar zxf "${go_version}.tar.gz" -C /usr/local
+)
+
+rm_if_exists "/tmp/${go_version}.tar.gz"
